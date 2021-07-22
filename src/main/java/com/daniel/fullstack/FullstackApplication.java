6d4bc6ca0138770181bb39f012580a1218fcc4ee
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-07-22 14:54:46 -0300

    Instânciada a classe Livro no BD e gerada a sua entidade

--- src/main/java/com/daniel/fullstack/FullstackApplication.java
+++ src/main/java/com/daniel/fullstack/FullstackApplication.java
@@ -5,15 +5,21 @@ package com.daniel.fullstack;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.daniel.fullstack.domain.Livro;
+import com.daniel.fullstack.repositories.LivroRepository;
 
 @SpringBootApplication
 public class FullstackApplication implements CommandLineRunner{
+	
+	@Autowired // Informa ao spring que será responsável por CRUD dessa instância
+	private LivroRepository livroRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FullstackApplication.class, args);
@@ -25,8 +31,9 @@ public class FullstackApplication implements CommandLineRunner{
 		
 		DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 		
-		Livro livro1 = new Livro(null, "Estudar", "Estudar Spring Boot 2 e Angular 11", LocalDateTime.parse("25/03/2022", formato) , null);
+		Livro livro1 = new Livro(null, "Estudar", "Estudar Spring Boot 2 e Angular 11", LocalDateTime.parse("25/03/2022 11:59", formato) , null);
 		
+		livroRepository.saveAll(Arrays.asList(livro1));
 	}
 
 }
