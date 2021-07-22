d62f1b687af61a6115da11714e468d7e7b93c99f
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-07-22 16:25:12 -0300

    Modificada a classe Principal

--- src/main/java/com/daniel/fullstack/FullstackApplication.java
+++ src/main/java/com/daniel/fullstack/FullstackApplication.java
@@ -3,37 +3,24 @@
  */
 package com.daniel.fullstack;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.daniel.fullstack.domain.Livro;
-import com.daniel.fullstack.repositories.LivroRepository;
-
 @SpringBootApplication
-public class FullstackApplication implements CommandLineRunner{
-	
-	@Autowired // Informa ao spring que será responsável por CRUD dessa instância
-	private LivroRepository livroRepository;
+public class FullstackApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FullstackApplication.class, args);
 	}
 
-	/** Método que sempre executa o código dentro sempre que a aplicação foir reiniciada */
+	/**
+	 * Método que sempre executa o código dentro sempre que a aplicação for
+	 * reiniciada
+	 */
 	@Override
 	public void run(String... args) throws Exception {
-		
-		DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
-		
-		Livro livro1 = new Livro(null, "Estudar", "Estudar Spring Boot 2 e Angular 11", LocalDateTime.parse("25/03/2022 11:59", formato) , null);
-		
-		livroRepository.saveAll(Arrays.asList(livro1));
+
 	}
 
 }
