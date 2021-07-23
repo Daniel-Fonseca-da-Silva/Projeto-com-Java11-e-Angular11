8376cb622509ee2d20caaef04ddc900e8500ed65
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-07-23 15:39:24 -0300

    Adicionado método que exibe todos os Livros do BD em LivroResource

--- src/main/java/com/daniel/fullstack/resources/LivroResource.java
+++ src/main/java/com/daniel/fullstack/resources/LivroResource.java
@@ -44,5 +44,6 @@ public class LivroResource {
 	@GetMapping
 	public ResponseEntity<List<Livro>> listAll() {
 		List<Livro> list = service.findAll();
+		return ResponseEntity.ok().body(list);
 	}
 }
