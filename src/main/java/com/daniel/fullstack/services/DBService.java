5ceeed67a9ac685b165119130db7dc17b0f6839c
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-07-23 12:39:32 -0300

    Modificada a classe DBService

--- src/main/java/com/daniel/fullstack/services/DBService.java
+++ src/main/java/com/daniel/fullstack/services/DBService.java
@@ -21,8 +21,11 @@ public class DBService {
 		DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 		
 		Livro livro1 = new Livro(null, "Estudar", "Estudar Spring Boot 2 e Angular 11", LocalDateTime.parse("25/03/2022 11:59", formato) , null);
+		Livro livro2 = new Livro(null, "Estudar", "Estudar Dinamarquês com Inglês", LocalDateTime.parse("01/02/2022 12:45", formato) , null);
+		Livro livro3 = new Livro(null, "Estudar", "Estudar lógica com C++", LocalDateTime.parse("31/05/2022 13:32", formato) , null);
+		Livro livro4 = new Livro(null, "Estudar", "Estudar javascript com HTML5 e CSS3", LocalDateTime.parse("17/08/2022 23:59", formato) , null);
 		
-		livroRepository.saveAll(Arrays.asList(livro1));
+		livroRepository.saveAll(Arrays.asList(livro1, livro2, livro3, livro4));
 	}
 
 }
