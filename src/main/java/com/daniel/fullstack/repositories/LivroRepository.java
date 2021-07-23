10ca27784bbaed871e2fdbf4362dd37ede7f8097
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-07-23 12:38:01 -0300

    Criado método em LivroRepository

--- src/main/java/com/daniel/fullstack/repositories/LivroRepository.java
+++ src/main/java/com/daniel/fullstack/repositories/LivroRepository.java
@@ -1,11 +1,17 @@
 package com.daniel.fullstack.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.daniel.fullstack.domain.Livro;
 
 @Repository
 public interface LivroRepository extends JpaRepository<Livro, Integer>{
+
+	@Query("SELECT obj FROM Livro obj WHERE obj.finalizado = null ORDER BY obj.dataParaFinalizar")
+	List<Livro> findAllOpen();
 
 }
