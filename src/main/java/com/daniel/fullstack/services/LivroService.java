452e275566abb974ae477ef285ede0718c15373c
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-07-23 12:37:23 -0300

    Criado método em LivroService

--- src/main/java/com/daniel/fullstack/services/LivroService.java
+++ src/main/java/com/daniel/fullstack/services/LivroService.java
@@ -1,5 +1,6 @@
 package com.daniel.fullstack.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,11 @@ public class LivroService {
 	public Livro findById(Integer id) {
 		Optional<Livro> obj = repository.findById(id);
 		return obj.orElse(null);
+	}
+
+	public List<Livro> findAllOpen() {
+		List<Livro> list = repository.findAllOpen();
+		return list;
 	}
 	
 }
